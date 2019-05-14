93b6fef58146eabba2496c22d0cef499c8ab13d1
Joel Corey <joelcorey@fastmail.com>
2019-05-14 05:47:09 -0700

    update readme

---
+++ README.md
@@ -0,0 +1,22 @@
+## Usage
+node phone.js via command line
+
+## Install
+npm i
+
+## Libraries used
+https://github.com/googlei18n/libphonenumber
+
+## Guidelines
+Write a function that accepts a user input phone number as a string and returns a normalized United States phone number 
+
+in E.164 format. Return null (or an equivalent value) if the user input is invalid.
+
+Use your preferred general purpose programming language.
+
+Write a readme.txt file that describes the code's functionality and why you decided on your solution.
+
+There is no time limit. Out of respect for your time, you can document the remainder of your solution if this problem 
+takes more than 1 hour.
+
+Document any online resources, third party libraries, and assumptions made when coding your solution.
--- phone.js
+++ phone.js
@@ -1,18 +1,6 @@
 //import { parsePhoneNumberFromString } from 'libphonenumber-js'
 const phone = require('libphonenumber-js')
 
-// Write a function that accepts a user input phone number as a string and returns a normalized United States phone number 
-// in E.164 format. Return null (or an equivalent value) if the user input is invalid.
-//
-// Use your preferred general purpose programming language.
-//
-// Write a readme.txt file that describes the code's functionality and why you decided on your solution.
-//
-// There is no time limit. Out of respect for your time, you can document the remainder of your solution if this problem 
-// takes more than 1 hour.
-//
-// Document any online resources, third party libraries, and assumptions made when coding your solution.
-
 function number(string) {
   const phoneNumber = phone.parsePhoneNumberFromString(string);
   const isValid = phoneNumber.formatInternational() === string;
